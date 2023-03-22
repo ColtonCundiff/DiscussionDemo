b3d141fe9c46e6ef41729efa3c3814e18856ebf5
Colton Cundiff <coltonrcundiff@gmail.com>
2023-03-21 22:49:37 -0400

    added

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+def encoder(string):
+    new_string = ''
+    for i in string:
+        new_string += str(int(i) + 3)
+
+    return new_string
+
+def decoder():
+    pass
+
+def main():
+    password = ''
+
+    while True:
+        print('Menu\n'
+              '----\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n')
+
+        option = input('Please enter an option: ')
+
+        if option == 1:
+            password = input('Please enter your password to encode: ')
+            password = encoder(password)
+            print('Your password has been encoded and stored!\n')
+
+        elif option == 2:
+            pass
+
+        elif option == 3:
+            break
+
+if __name__ == '__main__':
+    main()
 
